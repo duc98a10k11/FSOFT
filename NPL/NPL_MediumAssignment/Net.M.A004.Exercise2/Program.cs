0001f9a7a446f0ef72ca22a9cf75aed9a1442b3f
duc98a10k11 <duc98a10k11@users.noreply.github.com>
2021-06-23 16:50:18 +0700

    assignment2_npl

---
+++ NPL/NPL_MediumAssignment/Net.M.A004.Exercise2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.M.A004.Exercise2
+{
+    class Program
+    {
+        // Function that returns value of poly[0]x(n-1) +
+        // poly[1]x(n-2) + .. + poly[n-1]
+        static int horner(int[] poly, int n, int x)
+        {
+            // Initialize result
+            int result = poly[0];
+
+            // Evaluate value of polynomial
+            // using Horner's method
+            for (int i = 1; i < n; i++)
+                result = result * x + poly[i];
+
+            return result;
+        }
+
+        // Driver Code
+        public static void Main()
+        {
+            // Let us evaluate value of
+            // 2x3 - 6x2 + 2x - 1 for x = 3
+            int[] poly = { 2, -6, 2, -1 };
+            int x = 3;
+            int n = poly.Length;
+            Console.Write("Value of polynomial is "
+                                + horner(poly, n, x));
+        }
+    }
+}
---
+++ NPL/NPL_MediumAssignment/Net.M.A004/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.M.A004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            do
+            {
+                Console.Write("nhap x = ");
+            } while (!Int32.TryParse(Console.ReadLine(),out x));
+           
+            Console.Write(2*x*x*x -6*x*x + 2*x -1);
+
+            Console.ReadKey();
+
+        }
+    }
+}
