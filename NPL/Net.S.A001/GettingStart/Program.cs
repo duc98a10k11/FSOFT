2728924362115db15f3f6b011a64056ffb85107a
duc98a10k11 <duc98a10k11@users.noreply.github.com>
2021-06-23 16:16:18 +0700

    assignment1_npl_GetStart

---
+++ NPL/Net.S.A001/GettingStart/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Your name: ");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine($"Say hello {name}");
+            Console.ReadKey();
+        }
+    }
+}
