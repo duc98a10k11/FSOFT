e55d66751342dc34f3ded20f90b0fdb28b77157c
duc98a10k11 <duc98a10k11@users.noreply.github.com>
2021-06-29 22:59:38 +0700

    assignment day 4

---
+++ NPL/NPL_MediumAssignment/Net.M.A009.Exercise1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.M.A009.Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("Input:");
+            name = Console.ReadLine();
+
+            name = name.Trim().ToLower();
+            string[] items = name.Split(' ');
+            string result = "";
+            foreach (var item in items)
+            {
+                if (item != "")
+                {
+                    result += " " + item.Substring(0, 1).ToUpper() + item.Substring(1);
+                }
+                
+            }
+            name = result.Trim();
+            Console.WriteLine("Output:"+name);
+            Console.ReadKey();
+        }
+    }
+}
