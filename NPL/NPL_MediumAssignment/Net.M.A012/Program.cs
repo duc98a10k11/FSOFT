fd3062a31197743991369fce0982464b9ca03083
duc98a10k11 <duc98a10k11@users.noreply.github.com>
2021-06-29 22:32:22 +0700

    Assignment day 5

---
+++ NPL/NPL_MediumAssignment/Net.M.A012/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.M.A012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Named Arguments.
+            Console.WriteLine("Named Arguments");
+            Student student1 = new Student(Name: "Nguyen", Class: "lop1", Age: 12, Mark: Convert.ToDecimal(3.2));
+            student1.Graduate(Convert.ToDouble(student1.Mark));
+            Console.WriteLine(student1.ToString());
+            Console.WriteLine(student1.ToString("name,class,age,mark,grade"));
+
+            //Optional Arguments
+            Console.WriteLine("\n\n Optional Arguments");
+            Student student2 = new Student("Khuong", "3Net", 12);
+            student2.Graduate(Convert.ToDouble(student2.Mark));
+            Console.WriteLine(student2.ToString());
+            Console.WriteLine(student2.ToString("name,class,age,mark,grade,entryDate,relationship"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NPL/NPL_MediumAssignment/Net.M.A012/Student.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.M.A012
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Gender { get; set; }
+        public string Relationship { get; set; }
+        public DateTime EntryDate { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public decimal Mark { get; set; }
+        public string Grade { get; set; }
+
+        //contructor
+        public Student(string Name, string Class, int Age, decimal Mark)
+        {
+            this.Name = Name;
+            this.Class = Class;
+            this.Age = Age;
+            this.Mark = Mark;
+        }
+
+        public Student(string Name, string Class,
+            int Age,
+            DateTime? EntryDate = null,
+            string Relationship = "Single",
+            decimal Mark = 0, string Grade = "F"
+            )
+        {
+            this.Name = Name;
+            this.Class = Class;
+            this.Relationship = Relationship;
+            this.EntryDate = EntryDate ?? DateTime.Today;
+            this.Age = Age;
+            this.Mark = Mark;
+            this.Grade = Grade;
+        }
+
+        public void Graduate(double GradePoint)
+        {
+            if (GradePoint >= 0 && GradePoint < 1)
+            {
+                this.Grade = "F";
+            }
+            else if (GradePoint >= 1 && GradePoint < 2)
+            {
+                this.Grade = "D";
+            }
+            else if (GradePoint >= 2 && GradePoint < 2.3)
+            {
+                this.Grade = "C";
+            }
+            else if (GradePoint >= 2.3 && GradePoint < 2.7)
+            {
+                this.Grade = "C+";
+            }
+            else if (GradePoint >= 2.7 && GradePoint < 3.0)
+            {
+                this.Grade = "B-";
+            }
+            else if (GradePoint >= 3.0 && GradePoint < 3.3)
+            {
+                this.Grade = "B";
+            }
+            else if (GradePoint >= 3.3 && GradePoint < 3.7)
+            {
+                this.Grade = "B+";
+            }
+            else if (GradePoint >= 3.7 && GradePoint < 4)
+            {
+                this.Grade = "A-";
+            }
+            else if (GradePoint == 4)
+            {
+                this.Grade = "A";
+            }
+            else
+            {
+                Console.WriteLine("Not a GradePoint.");
+            }
+        }
+
+        public string ToString(string formatString = "name,grade")
+        {
+            string[] temp = formatString.Split(',');
+            string result = "";
+            foreach (var item in temp)
+            {
+                switch (item)
+                {
+                    case "name":
+                        result += " name: " + this.Name;
+                        break;
+                    case "class":
+                        result += " class: " + this.Class;
+                        break;
+                    case "gender":
+                        result += " gender: " + this.Gender;
+                        break;
+                    case "relationship":
+                        result += " relationship: " + this.Relationship;
+                        break;
+                    case "entryDate":
+                        result += " entryDate: " + this.EntryDate.ToShortDateString();
+                        break;
+                    case "age":
+                        result += " age: " + this.Age;
+                        break;
+                    case "address":
+                        result += " address: " + this.Address;
+                        break;
+                    case "mark":
+                        result += " mark: " + this.Mark;
+                        break;
+                    case "grade":
+                        result += " grade: " + this.Grade;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return result;
+        }
+    }
+}
